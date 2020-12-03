d25bdbf5b05c768e206538c05c002fbe44d06d57
Glenn Svendsen <Gsvend20@student.aau.dk>
2020-12-03 10:19:47 +0100

    init, ready for edits

---
+++ src/Input_main.cpp
@@ -0,0 +1,43 @@
+
+//  *** Author: Group B336  ***
+//  *** Project: Miniproject for P1***
+//  *** Node: --//--***
+//  *** Created: 03-12-2020***
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Pose.h"
+#include <sstream>
+
+//VARIBLES
+
+
+
+
+
+
+
+//FUNCTION CALLS
+
+
+
+
+
+using namespace std;
+
+
+//SUB CALLBACKS
+
+
+// Creating Publisher and subscriber 
+
+
+
+int main(int argc, char const *argv[])
+{
+    while(ros::ok()){
+
+
+    }
+    
+    return 0;
+}
