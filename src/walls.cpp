8547b14c6842b208a76daad91dedde2942cef1a3
SimonHaaningAAU <san20@student.aau.dk>
2020-12-21 12:23:24 +0100

    Wall turtles can move by pressing w/s or o/l but new input takes priority such that holding a key can be interrupted

--- src/walls.cpp
+++ src/walls.cpp
@@ -8,17 +8,17 @@
 
 #include <sstream> //Why is this here?
 
+#include <iostream>
+
+//Used for reading keyboard input
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
 //Used to setup turtles and get info on them
 #include <turtlesim/Spawn.h>
 #include <turtlesim/SetPen.h>
 #include <turtlesim/Pose.h>
-
-//Ascii codes for used keyboard characters (all lower case)
-//https://en.wikipedia.org/wiki/ASCII
-#define KEYCODE_UP 0x41
-#define KEYCODE_DOWN 0x42
-#define KEYCODE_W 0x77
-#define KEYCODE_S 0x73
 
 ros::NodeHandle *pointer_n;
 
@@ -32,16 +32,18 @@ void spawnWalls() //Spawning turtles to control
   wall_spawn_msg.request.name = "turtle2";
   wall_spawn_msg.request.x = 1.544445;
   wall_spawn_msg.request.y = 5.544445;
+  wall_spawn_msg.request.theta = M_PI/2;
   spawn_client.call(wall_spawn_msg);
 
   //Spawning second wall
   wall_spawn_msg.request.name = "turtle3";
   wall_spawn_msg.request.x = 9.544445;
   wall_spawn_msg.request.y = 5.544445;
+  wall_spawn_msg.request.theta = M_PI/2;
   spawn_client.call(wall_spawn_msg);
 }
 
-void stopDraw() //Stops the turtles from drawing lines when they move
+void stopDraw() //Stops the turtles from drawing lines when they move (maybe unneeded)
 {
   //Sets up two service clients, one for each wall turtle
   ros::ServiceClient pen_client2 = pointer_n->serviceClient<turtlesim::SetPen>("/turtle2/set_pen");
@@ -53,12 +55,40 @@ void stopDraw() //Stops the turtles from drawing lines when they move
   pen_client3.call(pen_srv);
 }
 
-//SUB CALLBACKS
+float speed(0.5); // Linear velocity (m/s)
+float turn(1.0); // Angular velocity (rad/s)
+float x(0), y(0), z(0), th(0); // Forward/backward/neutral direction vars
+char key(' ');
 
+//From https://github.com/methylDragon/teleop_twist_keyboard_cpp/blob/525fb108bbe7ad2527b82cc9ebc0d461b1bdadf3/src/teleop_twist_keyboard.cpp#L81
+//For non-blocking keyboard inputs
+int getch(void)
+{
+  int ch;
+  struct termios oldt;
+  struct termios newt;
 
-// Creating Publisher and subscriber 
+  // Store old settings, and copy to new settings
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
 
+  // Make required changes and apply the settings
+  newt.c_lflag &= ~(ICANON | ECHO);
+  newt.c_iflag |= IGNBRK;
+  newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
+  newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
+  newt.c_cc[VMIN] = 1;
+  newt.c_cc[VTIME] = 0;
+  tcsetattr(fileno(stdin), TCSANOW, &newt);
 
+  // Get the current character
+  ch = getchar();
+
+  // Reapply old settings
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+  return ch;
+}
 
 int main(int argc, char *argv[]){
 ros::init(argc,argv, "Walls");
@@ -67,10 +97,45 @@ ros::init(argc,argv, "Walls");
   pointer_n = &n;
 
   spawnWalls();
+  stopDraw();
+
+  geometry_msgs::Twist turtle2Twist;
+  geometry_msgs::Twist turtle3Twist;
+  ros::Publisher turtle2Move = n.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 1);
+  ros::Publisher turtle3Move = n.advertise<geometry_msgs::Twist>("/turtle3/cmd_vel", 1);
+
+  int key;
 
   while(ros::ok())
   {
+    key = getch();
+    switch (key)
+    {
+      case 119: //119 = w
+        turtle2Twist.linear.x = 1;
+        break;
 
+      case 115: //115 = s
+        turtle2Twist.linear.x = -1;
+        break;
+
+      case 111: //111 = o
+        turtle3Twist.linear.x = 1;
+        break;
+
+      case 108: //108 = l
+        turtle3Twist.linear.x = -1;
+        break;
+
+      case 3: //Exit program by pressing ctrl+c
+        return 0;
+
+      default:
+        std::cout << key << std::endl;
+    }
+    turtle2Move.publish(turtle2Twist);
+    turtle3Move.publish(turtle3Twist);
+    ros::spinOnce();
   }
   return 0;
 }
