369247c932dc5b8d7d1f38b7ea98914372a215f2
SimonHaaningAAU <san20@student.aau.dk>
2020-12-21 10:20:17 +0100

    Added class for keyboard reading (from turtlesim teleop)

--- src/Walls.cpp
+++ src/Walls.cpp
@@ -3,22 +3,162 @@
 //  *** Project: Miniproject for P1***
 //  *** Node: --//--***
 //  *** Created: 03-12-2020***
-
-//https://github.com/ros/ros_tutorials/blob/cd4ed467c7bc4b86512fed1f82346b95bf313a31/turtlesim/tutorials/teleop_turtle_key.cpp
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <turtlesim/Pose.h>
-#include <sstream>
+#include <geometry_msgs/Twist.h> //Turtlesim movement
 
+//Following inclusions are used for the KeyboardReader Class
+//https://github.com/ros/ros_tutorials/blob/cd4ed467c7bc4b86512fed1f82346b95bf313a31/turtlesim/tutorials/teleop_turtle_key.cpp
+#include <signal.h>
+#include <stdio.h>
+#ifndef _WIN32
+# include <termios.h>
+# include <unistd.h>
+#else
+# include <windows.h>
+#endif
 
+#include <sstream> //Why is this here?
+
+//Used to setup turtles and get info on them
 #include <turtlesim/Spawn.h>
 #include <turtlesim/SetPen.h>
-//VARIBLES
+#include <turtlesim/Pose.h>
 
+//Ascii codes for used keyboard characters (all lower case)
+//https://en.wikipedia.org/wiki/ASCII
+#define KEYCODE_UP 0x41
+#define KEYCODE_DOWN 0x42
+#define KEYCODE_W 0x77
+#define KEYCODE_S 0x73
+//#define KEYCODE_Q 0x71
 
+//KeyboardReader class from https://github.com/ros/ros_tutorials/blob/cd4ed467c7bc4b86512fed1f82346b95bf313a31/turtlesim/tutorials/teleop_turtle_key.cpp
+class KeyboardReader
+{
+public:
+  KeyboardReader()
+#ifndef _WIN32
+    : kfd(0)
+#endif
+  {
+#ifndef _WIN32
+    // get the console in raw mode
+    tcgetattr(kfd, &cooked);
+    struct termios raw;
+    memcpy(&raw, &cooked, sizeof(struct termios));
+    raw.c_lflag &=~ (ICANON | ECHO);
+    // Setting a new line, then end of file
+    raw.c_cc[VEOL] = 1;
+    raw.c_cc[VEOF] = 2;
+    tcsetattr(kfd, TCSANOW, &raw);
+#endif
+  }
+  void readOne(char * c)
+  {
+#ifndef _WIN32
+    int rc = read(kfd, c, 1);
+    if (rc < 0)
+    {
+      throw std::runtime_error("read failed");
+    }
+#else
+    for(;;)
+    {
+      HANDLE handle = GetStdHandle(STD_INPUT_HANDLE);
+      INPUT_RECORD buffer;
+      DWORD events;
+      PeekConsoleInput(handle, &buffer, 1, &events);
+      if(events > 0)
+      {
+        ReadConsoleInput(handle, &buffer, 1, &events);
+        if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_LEFT)
+        {
+          *c = KEYCODE_LEFT;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_UP)
+        {
+          *c = KEYCODE_UP;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_RIGHT)
+        {
+          *c = KEYCODE_RIGHT;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_DOWN)
+        {
+          *c = KEYCODE_DOWN;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x42)
+        {
+          *c = KEYCODE_B;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x43)
+        {
+          *c = KEYCODE_C;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x44)
+        {
+          *c = KEYCODE_D;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x45)
+        {
+          *c = KEYCODE_E;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x46)
+        {
+          *c = KEYCODE_F;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x47)
+        {
+          *c = KEYCODE_G;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x51)
+        {
+          *c = KEYCODE_Q;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x52)
+        {
+          *c = KEYCODE_R;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x54)
+        {
+          *c = KEYCODE_T;
+          return;
+        }
+        else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x56)
+        {
+          *c = KEYCODE_V;
+          return;
+        }
+      }
+    }
+#endif
+  }
+  void shutdown()
+  {
+#ifndef _WIN32
+    tcsetattr(kfd, TCSANOW, &cooked);
+#endif
+  }
+private:
+#ifndef _WIN32
+  int kfd;
+  struct termios cooked;
+#endif
+};
 
-
-
+KeyboardReader input; //
 
 ros::NodeHandle *pointer_n;
 
