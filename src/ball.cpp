e132e35b4e3a3ec35188da7791d7242213582b0f
Miki0110 <Mikipregaard@yahoo.dk>
2020-12-22 08:21:32 +0100

    gaming works

--- src/ball.cpp
+++ src/ball.cpp
@@ -10,7 +10,6 @@
 #include <turtlesim/Kill.h>  
 #include <turtlesim/SetPen.h>
 #include <turtlesim/TeleportAbsolute.h>
-#include <sstream>
 
 //class creation
 class ball{
@@ -26,8 +25,11 @@ public:
 ros::ServiceClient spawn_client = n.serviceClient<turtlesim::Spawn>("/spawn");
 ros::ServiceClient kill_client = n.serviceClient<turtlesim::Kill>("/kill");
 ros::ServiceClient pen_client = n.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
-ros::Subscriber pose_sub = n.subscribe("/turtle1/pose",100, &ball::callbackMyPose, this);
+ros::Subscriber my_pose_sub = n.subscribe("/turtle1/pose",100, &ball::callbackMyPose, this);
+ros::Subscriber left_pose_sub = n.subscribe("/turtle2/pose",100, &ball::distanceCheck, this);
+ros::Subscriber right_pose_sub = n.subscribe("/turtle3/pose",100, &ball::distanceCheck, this);
 turtlesim::Pose my_pose;
+bool contact = false;
 
 void move(double xSpeed, double turnSpeed){
     move_msg.linear.x = xSpeed;
@@ -43,6 +45,24 @@ void teleport(double posX, double posY, double angle){
 void callbackMyPose(const turtlesim::Pose& pose) {
    my_pose = pose;
 }
+void distanceCheck(const turtlesim::Pose& pose) {
+    double x_distance = my_pose.x - pose.x;
+    double y_distance = my_pose.y - pose.y;
+    double distance = hypot(x_distance, y_distance);
+
+    if (distance < 1.0){
+        contact = true;
+        }
+    else{
+        contact = false;
+        }
+}
+bool hitDetection(){
+    if(contact == true)
+    return true;
+    else
+    return false;
+}
 void respawn(){
     turtlesim::Kill kill_msg;
     kill_msg.request.name = "turtle1";
@@ -53,7 +73,6 @@ void respawn(){
     spawn_msg.request.x = 5.5;
     spawn_msg.request.y = 5.5;
     int randomAngle = rand() % 360 + 1;
-    ROS_INFO("Random angle: %d", randomAngle);
     double header = randomAngle*M_PI/180;
     spawn_msg.request.theta = header;
     spawn_client.call(spawn_msg);
@@ -91,17 +110,31 @@ turtlesim::SetPen pen_srv;
 pen_srv.request.off = true;
 moving_balls.pen_client.call(pen_srv);
 
+std::cout << "\n-------------------------------\nWelcome to Turtlepong!\n\n-------------------------------\nplayer 1 is located on the left \nplayer 2 is located on the right \n-------------------------------\n";
+int player1_points = 0;
+int player2_points = 0;
+
 for (int i = 0; i < 10; i++)
 {
     moving_balls.move(1.0,0.0);
     loop_rate.sleep();
 }
+printf("Current score is \nPlayer 1: %d \nPlayer 2: %d\n", player1_points, player2_points);
 
     while(ros::ok()){
-        if(moving_balls.my_pose.x > 10.9 || moving_balls.my_pose.x < 0.2){
+        if(moving_balls.my_pose.x > 10.9 &! moving_balls.my_pose.x == 0.0){
             moving_balls.respawn();
             ros::Duration(1.0).sleep(); //these are to fix potential bugs when a turtle is spawned 
             double speed = 1.0;
+            player1_points++;
+            printf("Current score is \nPlayer 1: %d \nPlayer 2: %d\n", player1_points, player2_points);
+        }
+        if(moving_balls.my_pose.x < 0.2 &! moving_balls.my_pose.x == 0.0){
+            moving_balls.respawn();
+            ros::Duration(1.0).sleep(); //these are to fix potential bugs when a turtle is spawned 
+            double speed = 1.0;
+            player2_points++;
+            printf("Current score is \nPlayer 1: %d \nPlayer 2: %d\n", player1_points, player2_points);
         }
 
         if (moving_balls.my_pose.y > 10.9 || moving_balls.my_pose.y < 0.1 &! moving_balls.my_pose.y == 0.0)
@@ -110,9 +143,10 @@ for (int i = 0; i < 10; i++)
             moving_balls.teleport(moving_balls.my_pose.x,moving_balls.my_pose.y, targetAngle);
             ros::Duration(0.5).sleep();//these are to fix potential bugs when a turtle is spawned in
         }
-
-        if(contact == true){
-            double targetAngle = M_PI*2 - moving_balls.my_pose.theta;
+        //&! moving_balls.my_pose.x == 0.0 &! moving_balls.my_pose.x > 10.9
+        if(moving_balls.hitDetection() != false){
+            std::cout << "hit\n";
+            double targetAngle = M_PI - moving_balls.my_pose.theta;
             moving_balls.teleport(moving_balls.my_pose.x,moving_balls.my_pose.y, targetAngle);
             ros::Duration(0.5).sleep();//these are to fix potential bugs when a turtle is spawned in
             speed = speed + 0.1;
--- src/walls.cpp
+++ src/walls.cpp
@@ -1,54 +1,87 @@
-
-//  *** Author: Group B336  ***
 //  *** Project: Miniproject for P1***
 //  *** Node: --//--***
 //  *** Created: 03-12-2020***
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
-#include "turtlesim/Pose.h"
-#include <sstream>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h> //Turtlesim movement
+
+//#include <sstream> //Why is this here?
+
+#include <iostream>
+
+//Used for reading keyboard input
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+//Used to setup turtles and get info on them
 #include <turtlesim/Spawn.h>
+#include <turtlesim/SetPen.h>
+#include <turtlesim/Pose.h>
 
-//VARIBLES
+ros::NodeHandle *pointer_n;
 
-
-
-
-
-
-ros::NodeHandle * pointer_n;
-
-//FUNCTION CALLS
-void spawn_Walls()
+void spawnWalls() //Spawning turtles to control
 {
+
   ros::ServiceClient spawn_client = pointer_n->serviceClient<turtlesim::Spawn>("/spawn");
   turtlesim::Spawn wall_spawn_msg;
 
   //Spawning first wall
   wall_spawn_msg.request.name = "turtle2";
-  wall_spawn_msg.request.x = 9;
-  wall_spawn_msg.request.y = 5;
+  wall_spawn_msg.request.x = 1.544445;
+  wall_spawn_msg.request.y = 5.544445;
+  wall_spawn_msg.request.theta = M_PI/2;
   spawn_client.call(wall_spawn_msg);
 
   //Spawning second wall
   wall_spawn_msg.request.name = "turtle3";
-  wall_spawn_msg.request.x = 9;
-  wall_spawn_msg.request.y = 5;
+  wall_spawn_msg.request.x = 9.544445;
+  wall_spawn_msg.request.y = 5.544445;
+  wall_spawn_msg.request.theta = M_PI/2;
   spawn_client.call(wall_spawn_msg);
 }
 
+void stopDraw() //Stops the turtles from drawing lines when they move (maybe unneeded)
+{
+  //Sets up two service clients, one for each wall turtle
+  ros::ServiceClient pen_client2 = pointer_n->serviceClient<turtlesim::SetPen>("/turtle2/set_pen");
+  ros::ServiceClient pen_client3 = pointer_n->serviceClient<turtlesim::SetPen>("/turtle3/set_pen");
 
+  turtlesim::SetPen pen_srv;
+  pen_srv.request.off = true;
+  pen_client2.call(pen_srv);
+  pen_client3.call(pen_srv);
+}
 
+//From https://github.com/methylDragon/teleop_twist_keyboard_cpp/blob/525fb108bbe7ad2527b82cc9ebc0d461b1bdadf3/src/teleop_twist_keyboard.cpp#L81
+//For non-blocking keyboard inputs
+int getch(void)
+{
+  int ch;
+  struct termios oldt;
+  struct termios newt;
 
-using namespace std;
+  // Store old settings, and copy to new settings
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
 
+  // Make required changes and apply the settings
+  newt.c_lflag &= ~(ICANON | ECHO);
+  newt.c_iflag |= IGNBRK;
+  newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
+  newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
+  newt.c_cc[VMIN] = 1;
+  newt.c_cc[VTIME] = 0;
+  tcsetattr(fileno(stdin), TCSANOW, &newt);
 
-//SUB CALLBACKS
+  // Get the current character
+  ch = getchar();
 
+  // Reapply old settings
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 
-// Creating Publisher and subscriber 
-
-
+  return ch;
+}
 
 int main(int argc, char *argv[]){
 ros::init(argc,argv, "Walls");
@@ -56,10 +89,50 @@ ros::init(argc,argv, "Walls");
   ros::NodeHandle n;
   pointer_n = &n;
 
-    while(ros::ok()){
+  spawnWalls();
+  stopDraw();
 
+  geometry_msgs::Twist turtle2Twist;
+  geometry_msgs::Twist turtle3Twist;
+  ros::Publisher turtle2Move = n.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 1);
+  ros::Publisher turtle3Move = n.advertise<geometry_msgs::Twist>("/turtle3/cmd_vel", 1);
 
+  int key;
+  printf("Control turtles with w/s and o/l \n\nPress ctrl+c to quit\n");
+
+  while(ros::ok())
+  {
+    key = getch();
+    switch (key)
+    {
+      case 119: //119 = w
+        turtle2Twist.linear.x = 1;
+        turtle2Move.publish(turtle2Twist);
+        break;
+
+      case 115: //115 = s
+        turtle2Twist.linear.x = -1;
+        turtle2Move.publish(turtle2Twist);
+        break;
+
+      case 111: //111 = o
+        turtle3Twist.linear.x = 1;
+        turtle3Move.publish(turtle3Twist);
+        break;
+
+      case 108: //108 = l
+        turtle3Twist.linear.x = -1;
+        turtle3Move.publish(turtle3Twist);
+        break;
+
+      case 3: //Exit program by pressing ctrl+c
+        return 0;
+
+      default:
+        std::cout << "Error! Wrong key pressed(" << key << ")" << std::endl
+        << "Press w/s to control first turtle or o/l to control the second." << std::endl;
     }
-    
-    return 0;
+    ros::spinOnce();
+  }
+  return 0;
 }
